d13e20a3055f3c404e46217265e7577286bdb5e0
Rohit Vishwakarma <rohitvishwakarma1819@gmail.com>
2024-08-04 23:44:54 +0530

    add player name editing feature, concept: two-way binding
    concept: two-way binding

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ function App() {
     <main>
       <div id="game-container">
         <ol id="players">
-          <Player name={"Player1"} symbol={"X"} />
-          <Player name={"Player2"} symbol={"O"} />
+          <Player initialName={"Player1"} symbol={"X"} />
+          <Player initialName={"Player2"} symbol={"O"} />
         </ol>
         Game board
       </div>
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,17 +1,28 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
 
   function handleEditClick() {
     setIsEditing((editing) => !editing);
   }
 
+  function handlePlayerNameChange(event) {
+    setPlayerName(event.target.value);
+  }
+
   return (
     <li>
       <span className="player">
-        {!isEditing && <span className="player-name">{name}</span>}
-        {isEditing && <input type="text" value={name}></input>}
+        {!isEditing && <span className="player-name">{playerName}</span>}
+        {isEditing && (
+          <input
+            type="text"
+            value={playerName}
+            onChange={handlePlayerNameChange}
+          ></input>
+        )}
         <span className="player-symbol">{symbol}</span>
       </span>
 
