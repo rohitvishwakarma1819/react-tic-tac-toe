66130f7a2a53b030adc7c46d2e8fd7ebedb3026e
Rohit Vishwakarma <rohitvishwakarma1819@gmail.com>
2024-08-05 00:04:39 +0530

    add game board UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
 function App() {
   return (
     <main>
@@ -7,7 +8,7 @@ function App() {
           <Player initialName={"Player1"} symbol={"X"} />
           <Player initialName={"Player2"} symbol={"O"} />
         </ol>
-        Game board
+        <GameBoard />
       </div>
     </main>
   );
